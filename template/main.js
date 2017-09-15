d532713273dc45a6a837778e86d353f2e138e7a1
HaykoKoryun <haykokoryun@gmail.com>
2017-09-15 18:04:33 +0400

    made template include save button

--- template/main.js
+++ template/main.js
@@ -2,11 +2,23 @@ document.addEventListener("magicbox.init", function(e)
 {
 	var manifest = e.detail.manifest;
 	var container = e.detail.container;
+	var data = e.detail.data;
+	var save = e.detail.save;
 
-	var p = document.createElement("p");
-	p.innerText = "Hello World";
+	var spinner = document.createElement("div");
+	spinner.id = "spinner";
+	spinner.style.backgroundImage = "url(" + manifest.assets["spinner"] + ")";
 
-	container.appendChild(p);
+	var saveButton = document.createElement("div");
+	saveButton.id = "save";
+	saveButton.innerText = "SAVE";
+	saveButton.addEventListener("click", function()
+	{
+		save(Math.round(Math.random() * 100));
+	});
+
+	container.appendChild(spinner);
+	container.appendChild(saveButton);
 
 	e.detail.ready();
 });
